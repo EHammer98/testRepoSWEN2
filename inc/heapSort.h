275e78cccc1016dcd27bc975d67fef74fc920650
SvanLeeuwen5 <s.vanleeuwen5@student.avans.nl>
2021-11-30 13:12:25 +0100

    heapSort

---
+++ inc/heapSort.h
@@ -0,0 +1,10 @@
+#pragma once
+class heapSort
+{
+public:
+    void swap(int*, int*);
+    void heapify(int arr[],int n, int i);
+    void sort(int arr[]);
+
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 
 #include "cli.h"
 #include "bubbleSort.h"
+#include "heapSort.h"
 #include <iostream>
 
 int main() {
 	cli print;
 	bubbleSort sort1;
-	int arrayToSort[100];
+	heapSort sort3;
+	int arrayToSort1[100];
+	int arrayToSort3[100];
 	for (int i = 0; i < 100; i++) {
-		arrayToSort[i] = rand() % 100;
+		arrayToSort1[i] = rand() % 100;
+		arrayToSort3[i] = rand() % 100;
 	}
-	sort1.sort(arrayToSort);
-	print.showArray(arrayToSort);
+	sort1.sort(arrayToSort1);
+	sort3.sort(arrayToSort3);
+	std::cout << "Bubblesort" << std::endl;
+	print.showArray(arrayToSort1);
+	std::cout << "Heapsort" << std::endl;
+	print.showArray(arrayToSort3);
+
 	return 0;
 }
 
