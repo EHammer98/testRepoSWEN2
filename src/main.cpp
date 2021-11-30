958cefb026377620bce857add9de88cf8a7f277f
Elwin Hammer <elwinhammer@gmail.com>
2021-11-30 14:05:52 +0100

    Merge and heap sort added and tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 
 #include "cli.h"
 #include "bubbleSort.h"
+#include "mergeSort.h"
 #include "heapSort.h"
 #include <iostream>
 
 int main() {
 	cli print;
 	bubbleSort sort1;
+	mergeSort sort2;
 	heapSort sort3;
-	int arrayToSort1[100];
+	int arrayToSort[100];
+	int arrayToSort2[100];
 	int arrayToSort3[100];
 	for (int i = 0; i < 100; i++) {
-		arrayToSort1[i] = rand() % 100;
+		arrayToSort[i] = rand() % 100;
+		arrayToSort2[i] = rand() % 100;
 		arrayToSort3[i] = rand() % 100;
 	}
-	sort1.sort(arrayToSort1);
+	sort1.sort(arrayToSort);
+	sort2.sort(arrayToSort2, 0, 99);
 	sort3.sort(arrayToSort3);
 	std::cout << "Bubblesort" << std::endl;
-	print.showArray(arrayToSort1);
+	print.showArray(arrayToSort);
+	std::cout << "Mergesort" << std::endl;
+	print.showArray(arrayToSort2);
 	std::cout << "Heapsort" << std::endl;
 	print.showArray(arrayToSort3);
-
 	return 0;
 }
 
